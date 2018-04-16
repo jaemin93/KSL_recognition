537493e39b9f269808be6d353c86a631514f9b18
jaemin93 <icebanana93@gmail.com>
2018-04-16 18:01:16 +0900

    desktop

--- classify_webcam.py
+++ classify_webcam.py
@@ -12,6 +12,18 @@ os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
 
 def predict(image_data):
+    global consonant
+    consonant = {'noinput': 0, 'giyeok':1, 'nieun': 2, 'diguek':3, 'rieul': 4, 'mieum':5, 
+                'bieup':6, 'sieuh':7,'o': 8, 'jieuh': 9, 'chieuh':10, 'kieuk':11,
+                'tiguek':12, 'pieup':13, 'hieuh':14}
+    global vowel
+    vowel = {'ah': 16, 'ya':17, 'uh':18, 'yeo': 19, 'oh':20, 'yo':21,
+            'woo':22, 'you':23, 'euh':24, 'yee':25}
+    global korean_dict
+    korean_dict = {'안':[8, 16, 2], '영':[8, 19, 8], '연':[8, 19, 2],
+                    '난':[2, 16, 2], '녕':[2, 19, 8], '년':[2, 19, 2], 
+                    '산':[7, 16, 2], '셩':[7, 19, 8], '션':[7, 19, 2],
+                    '잔':[9, 16, 2], '졍':[9, 19, 8], '젼':[9, 19, 2]}
 
     predictions = sess.run(softmax_tensor, \
              {'DecodeJpeg/contents:0': image_data})
@@ -49,9 +61,13 @@ with tf.Session() as sess:
 
     res, score = '', 0.0
     i = 0
+    key = list()
+    cnt = 0
     mem = ''
     consecutive = 0
     sequence = ''
+    tmp = ''
+    
     while True:
         ret, img = cap.read()
         img = cv2.flip(img, 1)
@@ -76,8 +92,45 @@ with tf.Session() as sess:
                     elif res == 'del':
                         sequence = sequence[:-1]
                     else:
-                        sequence += res
+                        sequence += res + " "
+                        cnt += 1
+                    if cnt == 3:
+                        cnt = 0
+                        sequence = sequence.split()
+                        for j in range(len(sequence)):
+                            try:
+                                if j is 0:   
+                                    key.append(consonant[sequence[j]])
+                                elif j is 1:
+                                    key.append(vowel[sequence[j]])
+                                else:
+                                    key.append(consonant[sequence[j]])
+                            except:
+                                #cv2.VideoCapture(0).release()
+                                break
+                        check = 0
+
+                        for write, syllable in korean_dict.items():
+                            try:
+                                if key[0] == syllable[0]:
+                                    check += 1
+                                if key[1] == syllable[1]:
+                                    check += 1 
+                                if key[2] == syllable[2]:
+                                    check += 1
+                                if check == 3:
+                                    print(write),
+                                    break
+                                else:
+                                    check = 0
+                            except:
+                                break                                         
+                        key.clear()
+                        sequence = ''
                     consecutive = 0
+                elif a == 27:
+                    cv2.VideoCapture(0).release()
+            ' '.join(sequence)
             i += 1
             cv2.putText(img, '%s' % (res.upper()), (100,400), cv2.FONT_HERSHEY_SIMPLEX, 4, (255,255,255), 4)
             cv2.putText(img, '(score = %.5f)' % (float(score)), (100,450), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255))
@@ -89,6 +142,3 @@ with tf.Session() as sess:
             cv2.imshow('sequence', img_sequence)
         else:
             break
-
-
-cv2.VideoCapture(0).release()
